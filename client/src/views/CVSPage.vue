e7cff534347d67aa1ddcc2f8e128189ac5436e1b
kkoomin <kkooming2@gmail.com>
2020-06-25 17:44:19 +0900

    create cvs table layout

--- client/src/views/CVSPage.vue
+++ client/src/views/CVSPage.vue
@@ -1,5 +1,30 @@
 <template>
-  <section class="cvs-page"></section>
+  <section class="cvs-page">
+    <h1>편의점 조합</h1>
+    <!-- 게시판 -->
+    <table class="cvs-table">
+      <thead>
+        <tr>
+          <th class="post-num">글번호</th>
+          <th class="post-title">제목</th>
+          <th class="post-author">작성자</th>
+          <th class="post-likes">좋아요</th>
+          <th class="post-views">조회수</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>마크정식아냐</td>
+          <td>익명</td>
+          <td>1</td>
+          <td>4</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- 글쓰기 -->
+    <button class="main-btn">글쓰기</button>
+  </section>
 </template>
 
 <script>
@@ -8,4 +33,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "../scss/main.scss";
+</style>

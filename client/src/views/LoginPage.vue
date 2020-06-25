2e251c04aa3366405bf3965fd0c4be5e49b234cf
kkoomin <kkooming2@gmail.com>
2020-06-25 21:14:41 +0900

    create kakao login button

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,11 +1,36 @@
 <template>
-  <section class="login-page"></section>
+  <section class="login-page">
+    <div class="login">
+      <h1>Login</h1>
+      <p class="login-welcome-message">
+        🙌🏻 안녕하세요, Lunch Strange입니다. 🙌🏻
+      </p>
+      <img
+        class="kakao-login-btn"
+        src="@/assets/kakao_login_large_wide.png"
+        alt="Kakao login button"
+        @click="kakaoLogin"
+      />
+      <p class="login-info">
+        Lunch Strange는 고객의 편의를 위해 카카오 로그인을 지원합니다.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "LoginPage",
+  methods: {
+    kakaoLogin: () => {
+      const kakao_app_key = process.env.VUE_APP_KAKAO_API_KEY;
+      const redirect_uri = "http://localhost:8080/";
+      location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao_app_key}&redirect_uri=${redirect_uri}&response_type=code`;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "../scss/LoginPage.scss";
+</style>

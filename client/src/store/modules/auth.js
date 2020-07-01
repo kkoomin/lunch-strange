635ce0579567c67fbf7b9e9ec060b993318cafd4
kkoomin <kkooming2@gmail.com>
2020-07-01 19:45:51 +0900

    add graphql query for filter

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -1,36 +1,34 @@
-// import qs from "qs";
+import qs from "qs";
 import router from "@/router";
-// import cookies from "vue-cookies";
-import axios from "axios";
+import cookies from "vue-cookies";
 
-const state = { token: "" };
+const state = { token: null };
 const getters = { isLoggedIn: (state) => !!state.token };
 const mutations = { setToken: (state, token) => (state.token = token) };
 const actions = {
   logout: ({ commit }) => {
-    // const kakao_app_key = process.env.VUE_APP_KAKAO_API_KEY;
-    // const redirect_uri = "http://localhost:8080/";
-    // const fullUrl = `https://kauth.kakao.com/oauth/logout?client_id=${kakao_app_key}?&logout_redirect_uri=${redirect_uri}`;
-    // window.open(fullUrl);
+    const kakao_app_key = process.env.VUE_APP_KAKAO_API_KEY;
+    const redirect_uri = process.env.VUE_APP_SERVER_URL + "/auth/logout";
+    const fullUrl = `https://kauth.kakao.com/oauth/logout?client_id=${kakao_app_key}&logout_redirect_uri=${redirect_uri}`;
+    location.href = fullUrl;
 
+    cookies.remove("k_token");
     commit("setToken", null);
-    location.pathname = "/";
   },
   login: () => {
     const kakao_app_key = process.env.VUE_APP_KAKAO_API_KEY;
-    const redirect_uri = process.env.VUE_APP_SERVER_URL;
+    const redirect_uri = process.env.VUE_APP_SERVER_URL + "/auth";
     const fullUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao_app_key}&redirect_uri=${redirect_uri}&response_type=code`;
     location.href = fullUrl;
   },
   finalizeLogin({ commit }) {
-    axios.post("http://70.12.224.218:8000/auth2", {}).then((res) => {
-      console.log(res.data.result);
-      const token = res.data.result;
-      if (token) {
-        commit("setToken", token);
-      }
-      router.push("/");
-    });
+    console.log(qs.parse(location.search.replace("?", "")).token);
+    const token = qs.parse(location.search.replace("?", "")).token;
+    if (token) {
+      cookies.set("k_token", token);
+      commit("setToken", token);
+    }
+    router.push("/");
   },
 };
 

59a23876624ea4e67de0bb57579549b74f228a6d
kkoomin <kkooming2@gmail.com>
2020-07-05 00:42:30 +0900

    edit auth login

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -1,6 +1,7 @@
 import qs from "qs";
 import router from "@/router";
 import cookies from "vue-cookies";
+import axios from "axios";
 
 const state = { token: null };
 const getters = { isLoggedIn: (state) => !!state.token };
@@ -8,7 +9,7 @@ const mutations = { setToken: (state, token) => (state.token = token) };
 const actions = {
   logout: ({ commit }) => {
     const kakao_app_key = process.env.VUE_APP_KAKAO_API_KEY;
-    const redirect_uri = process.env.VUE_APP_SERVER_URL + "/auth/logout";
+    const redirect_uri = process.env.VUE_APP_SERVER_URL + "auth/logout";
     const fullUrl = `https://kauth.kakao.com/oauth/logout?client_id=${kakao_app_key}&logout_redirect_uri=${redirect_uri}`;
     location.href = fullUrl;
 
@@ -17,18 +18,27 @@ const actions = {
   },
   login: () => {
     const kakao_app_key = process.env.VUE_APP_KAKAO_API_KEY;
-    const redirect_uri = process.env.VUE_APP_SERVER_URL + "/auth";
+    const redirect_uri = process.env.VUE_APP_SERVER_URL + "auth";
     const fullUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao_app_key}&redirect_uri=${redirect_uri}&response_type=code`;
     location.href = fullUrl;
   },
   finalizeLogin({ commit }) {
-    console.log(qs.parse(location.search.replace("?", "")).token);
+    console.log(qs.parse(location.search.replace("?", "")));
     const token = qs.parse(location.search.replace("?", "")).token;
     if (token) {
       cookies.set("k_token", token);
       commit("setToken", token);
+      // commit("getUserInfo", token)
     }
+    // window.location.href = "https://lunch-strange.netlify.app/";
     router.push("/");
+  },
+  async getUserInfo(token) {
+    const result = axios.post(
+      `${process.env.VUE_APP_SERVER_URL}auth/getUserInfo`,
+      { token }
+    );
+    console.log(result);
   },
 };
 

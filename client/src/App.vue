a60b8a6c2bd8bc4a8ece9e5395911b00dbcd797a
kkoomin <kkooming2@gmail.com>
2020-07-05 18:08:09 +0900

    add keep login funtion

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,10 +9,22 @@
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import cookies from "vue-cookies";
+import { mapActions } from "vuex";
 
 export default {
   name: "App",
   components: { Header, Footer },
+  methods: {
+    ...mapActions(["keepLogin"]),
+  },
+  created() {
+    // 새로고침시 로그인 유지
+    if (cookies.get("u_id") && cookies.get("k_token")) {
+      const token = cookies.get("k_token");
+      this.keepLogin(token);
+    }
+  },
 };
 </script>
 
